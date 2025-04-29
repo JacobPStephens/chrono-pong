2cf5bca16229e6313ca1f2aea4cd088f76814797
Zachary Mercado <mercadoz2@southernct.edu>
2025-04-28 22:44:51 -0400

    added comment

--- Assets/Scripts/ball_collision.cs
+++ Assets/Scripts/ball_collision.cs
@@ -12,7 +12,7 @@ public class ball_collision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // ut oh spagetti oh
     }
 
     // Update is called once per frame
