e3bf6e1566c8ae59b92a37ee4f45fead6d3ba869
Zachary Mercado <mercadoz2@southernct.edu>
2025-04-28 22:56:20 -0400

    added comment

---
+++ Assets/Scripts/ball_collision.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_collision : MonoBehaviour
+{
+
+    public debug debugScript;
+
+    public GameObject net;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ut oh spagetti oh
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionExit(Collision collisionInfo){
+        if(collisionInfo.gameObject.name == "net" && transform.position.z >= 0.331f){
+            debugScript.LaunchBall();
+        }
+    }
+}
