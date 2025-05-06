b01e4bd2cc138d02f0989b9d9a961e4406f0a531
JacobPStephens <jacobpstephens9@gmail.com>
2025-05-05 22:58:28 -0400

    basic lighting

---
+++ Assets/Scripts/light.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Material mat = GetComponent<Material>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
