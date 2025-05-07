88743931d6a9522cba9e2d54ac567b73fdf6dab1
Zachary Mercado <mercadoz2@southernct.edu>
2025-05-06 23:38:27 -0400

    Added main menu scene and started on the input

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    //public InputActionAsset inputActions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // public void PressRightTrigger(InputAction.CallbackContext context) {
+    //     if (context.performed) { 
+    //         SceneManager.LoadScene("Ping Pong", LoadSceneMode.Single);
+    //     }
+    // }
+}
